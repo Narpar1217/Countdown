28eea45e8faccc4e66ad5c284dc2cb7aa825faac
Adam Beagle <abeagle88@gmail.com>
2015-03-18 11:21:50 -0400

    Add setup.py, update .gitignore to ignore egg-info

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Countdown',
+    version='0.1',
+    description='Console-based game similar to the gameshow Countdown.',
+    author='Adam Beagle',
+    packages=['countdown'],
+)
