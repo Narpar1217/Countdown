793c40959f237657fae68c130a259a810235f554
Adam Beagle <abeagle88@gmail.com>
2015-03-18 15:55:00 -0400

    Add LettersRound class and test First pass. Exposed attributes in LettersRound will need to change later to suit what's needed and what isn't.

--- countdown/game/lettersround.py
+++ countdown/game/lettersround.py
@@ -1,22 +1,39 @@
 """
-lettersgame.py
+lettersround.py
 Author: Adam Beagle
 """
-from random import choice
+from random import randrange
 
 from .resmaps import LETTER_FREQUENCY_PATH, WORDLIST_PATH
 
-def build_letter_frequency():
+def build_letter_frequency(path):
+    """
+    Return dictionary whose keys are each letter of the alphabet, and whose
+    values are the number of times each letter can appear in its respective
+    pile.
+
+    'path' is assumed to point to a file with each line formatted
+    [letter][one space][number]
+
+    For example, a portion of the file may look as such:
+    
+    a 10
+    b 5
+    c 4
+    """
     freq_map = {}
     
-    with open(LETTER_FREQUENCY_PATH) as f:
+    with open(path) as f:
         for line in f:
             char, freq = line.split(' ')
-            freq_map[char] = freq
+            freq_map[char] = int(freq)
 
     return freq_map
 
 class InitError(Exception):
+    pass
+
+class LettersRoundError(Exception):
     pass
 
 class WordList:
@@ -55,7 +72,6 @@ class WordList:
             raise InitError(
                 "WordList.init() must be called before search() can be used."
             )
-            
 
     @staticmethod
     def _build_from_file(path):
@@ -69,3 +85,95 @@ class WordList:
 
         return wordlist
 
+class LettersRound:
+    """
+    All public methods return (success code, message) where `success code`
+    is a boolean representing general success or failure of a given operation,
+    and `message` provides more information to the user, if applicable (message
+    can be empty).
+    
+    METHODS:
+      add_consonant
+      add_vowel
+      verify_word
+    """
+    wordlist = WordList()
+    letter_frequency = build_letter_frequency(LETTER_FREQUENCY_PATH)
+    max_letters = 9
+
+    min_vowels = 3
+    min_consonants = 4
+    
+    max_consonants = max_letters - min_vowels
+    max_vowels = max_letters - min_consonants
+    
+    def __init__(self):
+        self.wordlist.init(WORDLIST_PATH)
+        self.consonants, self.vowels = self._init_letter_bags()
+        self.showing = []
+        self.num_consonants = 0
+        self.num_vowels = 0
+
+    def add_consonant(self):
+        if self.num_consonants < self.max_consonants:
+            i = randrange(len(self.consonants))
+            self._add_to_showing(self.consonants.pop(i))
+            self.num_consonants += 1
+            
+            return True, ''
+
+        return False, 'Minimum {} vowels required'.format(self.min_vowels)
+
+    def add_vowel(self):
+        if self.num_vowels < self.max_vowels:
+            i = randrange(len(self.vowels))
+            self._add_to_showing(self.vowels.pop(i))
+            self.num_vowels += 1
+            
+            return True, ''
+
+        return False, 'Minimum {} consonants required'.format(
+            self.min_consonants
+        )
+    
+    def verify_word(self, word):
+        word = word.upper()
+        remaining = self.showing[:]
+
+        # Verify word can be made from available letters
+        for letter in word:
+            if letter in remaining:
+                remaining.remove(letter)
+            else:
+                return False, 'Sorry, word cannot be made from given letters.'
+
+        # Verify word in dictionary
+        if self.wordlist.search(word):
+            return True, "Word is valid!"
+        else:
+            return False, "Sorry, your word was not found in the dictionary."
+
+    def _add_to_showing(self, letter):
+        if self.showing_full:
+            raise LettersRoundError(
+                "max_letters reached. Cannot add another letter."
+            )
+
+        self.showing.append(letter)
+
+    def _init_letter_bags(self):
+        freq = self.letter_frequency
+        vowels = []
+        consonants = []
+        
+        for letter in freq:
+            if letter in ('A', 'E', 'I', 'O', 'U'):
+                vowels += freq[letter]*[letter]
+            else:
+                consonants += freq[letter]*[letter]
+
+        return consonants, vowels
+
+    @property
+    def showing_full(self):
+        return len(self.showing) == self.max_letters
---
+++ tests/test_lettersround.py
@@ -0,0 +1,66 @@
+import pytest
+
+from countdown.game.lettersround import LettersRound, LettersRoundError
+
+@pytest.fixture
+def consonants():
+    return [c.upper() for c in 'bcdfghjklmnpqrstvwxyz']
+
+@pytest.fixture
+def vowels():
+    return [v.upper() for v in 'aeiou']
+
+def test_basic(consonants, vowels):
+    lr = LettersRound()
+
+    for i in range(lr.max_consonants):
+        assert lr.add_consonant()[0] == True
+        assert lr.showing[-1] in consonants
+        assert not lr.showing_full
+
+    for i in range(lr.min_vowels):
+        assert lr.add_vowel()[0] == True
+        assert lr.showing[-1] in vowels
+
+    assert lr.showing_full
+
+def test_too_many_vowels(vowels):
+    lr = LettersRound()
+    
+    for i in range(lr.max_vowels):
+        assert lr.add_vowel()[0] == True
+        assert lr.showing[-1] in vowels
+
+    assert lr.num_vowels == lr.max_vowels
+
+    assert lr.add_vowel()[0] == False
+
+def test_too_many_consonants(consonants):
+    lr = LettersRound()
+
+    for i in range(lr.max_consonants):
+        assert lr.add_consonant()[0] == True
+        assert lr.showing[-1] in consonants
+
+    assert lr.num_consonants == lr.max_consonants
+
+    assert lr.add_consonant()[0] == False
+
+def test_too_many_letters(consonants, vowels):
+    lr = LettersRound()
+    
+    # Fill showing
+    for i in range(lr.max_consonants):
+        lr.add_consonant()
+        assert lr.showing[-1] in consonants
+        assert not lr.showing_full
+
+    for i in range(lr.min_vowels):
+        lr.add_vowel()
+        assert lr.showing[-1] in vowels
+
+    assert lr.showing_full
+
+    # Try to add another letter
+    with pytest.raises(LettersRoundError):
+        lr.add_vowel()
